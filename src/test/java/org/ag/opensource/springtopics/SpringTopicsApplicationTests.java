789599fc9c3e47994c9b5c0f9f28e6d1bf6e7eaa
ankitgir <ankitgir@amdocs.com>
2024-03-08 14:37:20 +0530

    Project setup

---
+++ src/test/java/org/ag/opensource/springtopics/SpringTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ag.opensource.springtopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTopicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
