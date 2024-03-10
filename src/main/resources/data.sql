1b99743b54d568d201d779c7bcd31bbe74bf139f
ankitgir <ankitgir@amdocs.com>
2024-03-10 18:34:11 +0530

    Initailize DB with datasource

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO employee (id, name, age, department, created_by, created_date) VALUES (1, 'Ankit Giri', 27, 'IT', 'admin', current_timestamp);
-INSERT INTO employee (id, name, age, department, created_by, created_date) VALUES (2, 'Neha Singh', 26, 'IT', 'admin', current_timestamp);
-INSERT INTO employee (id, name, age, department, created_by, created_date) VALUES (3, 'Ankit Kumar', 25, 'Marketing', 'admin', current_timestamp);
-INSERT INTO employee (id, name, age, department, created_by, created_date) VALUES (4, 'Ankit Chauhan', 24, 'Finance', 'admin', current_timestamp);
-INSERT INTO employee (id, name, age, department, created_by, created_date) VALUES (5, 'Anjali Giri', 24, 'HR', 'admin', current_timestamp);
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee (name, age, department, created_by, created_date) VALUES ('Ankit Giri', 27, 'IT', 'admin', current_timestamp);
+INSERT INTO employee (name, age, department, created_by, created_date) VALUES ('Neha Singh', 26, 'IT', 'admin', current_timestamp);
+INSERT INTO employee (name, age, department, created_by, created_date) VALUES ('Ankit Kumar', 25, 'MARKETING', 'admin', current_timestamp);
+INSERT INTO employee (name, age, department, created_by, created_date) VALUES ('Ankit Chauhan', 24, 'FINANCE', 'admin', current_timestamp);
+INSERT INTO employee (name, age, department, created_by, created_date) VALUES ('Anjali Giri', 24, 'HR', 'admin', current_timestamp);
